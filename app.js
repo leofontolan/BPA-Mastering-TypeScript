897e6470af08a8769ffb41a5a556f107ec5dd7e6
Leo Fontolan <leofontolan@gmail.com>
2017-06-07 10:02:57 -0300

    Status contagem de conta a pagar

--- app.js
+++ app.js
@@ -7,14 +7,30 @@ var app = new Vue({
         title: "Contas a Pagar",
 
         bills: [
-            {date_due: '20/08/2017', name: 'Conta de Luz', value: '78,99'},
-            {date_due: '21/08/2017', name: 'Conta de Água', value: '123,99'},
-            {date_due: '22/08/2017', name: 'Conta de Telefone', value: '124,99'},
-            {date_due: '23/08/2017', name: 'Supermercado', value: '600,99'},
-            {date_due: '24/08/2017', name: 'Cartão de Crédito', value: '1599,99'},
-            {date_due: '25/08/2017', name: 'Empréstimo', value: '2500,99'},
-            {date_due: '26/08/2017', name: 'Gasolina', value: '345,00'}
+            {date_due: '20/08/2017', name: 'Conta de Luz', value: '78,99', done: 1},
+            {date_due: '21/08/2017', name: 'Conta de Água', value: '123,99', done: 0},
+            {date_due: '22/08/2017', name: 'Conta de Telefone', value: '124,99', done: 0},
+            {date_due: '23/08/2017', name: 'Supermercado', value: '600,99', done: 0},
+            {date_due: '24/08/2017', name: 'Cartão de Crédito', value: '1599,99', done: 0},
+            {date_due: '25/08/2017', name: 'Empréstimo', value: '2500,99', done: 0},
+            {date_due: '26/08/2017', name: 'Gasolina', value: '345,00', done: 0}
         ],
     },
+
+    computed: {
+
+        status: function () {
+
+            var count = 0;
+
+            for (var i in this.bills) {
+                if (!this.bills[i].done) {
+                    count++;
+                }
+            }
+
+            return !count ? "Nenhuma conta a pagar" : "Existem  " + count + " a serem pagas."
+        }
+    }
 
 });
