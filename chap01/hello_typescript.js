3aaad2bdc42df12ab3acab022f359c7baaef4248
leofontolan <leofontolan@gmail.com>
2022-05-23 14:25:20 -0300

    Duck Type

--- chap01/hello_typescript.js
+++ chap01/hello_typescript.js
@@ -1,2 +1,13 @@
+"use strict";
 var version = 'es6';
-console.log("Hello, ".concat(version, " TypeScript!"));
+console.log(`Hello, ${version} TypeScript!`);
+let myString = `this is a string`;
+console.log(myString);
+//Retorna um erro, se tentarmos atribuir um boolean em um tipo string
+//myString =  true;
+let inferredString = 'this is a string';
+console.log(typeof (inferredString));
+//Duck Type
+var nameIdObject = { name: "myName", id: 1, print() { } }; //Duck Type
+nameIdObject = { id: 2, name: "anotherName", print() { } }; // Não gera erro, pois o tipo é o mesmo
+// nameIdObject = { id: 3, name: "thirdName" }; //Gera erro, pois sem método print(), o tipo não é o mesmo
--- chap01/hello_typescript.ts
+++ chap01/hello_typescript.ts
@@ -1,3 +1,17 @@
 var version = 'es6';
 
 console.log(`Hello, ${version} TypeScript!`);
+
+let myString: string = `this is a string`;
+console.log(myString);
+
+//Retorna um erro, se tentarmos atribuir um boolean em um tipo string
+//myString =  true;
+
+let inferredString = 'this is a string';
+console.log(typeof(inferredString));
+
+//Duck Type
+var nameIdObject = { name: "myName", id: 1, print() { } }; //Duck Type
+nameIdObject = { id: 2, name: "anotherName", print() { } }; // Não gera erro, pois o tipo é o mesmo
+// nameIdObject = { id: 3, name: "thirdName" }; //Gera erro, pois sem método print(), o tipo não é o mesmo
